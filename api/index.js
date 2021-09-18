6b5d86d77eb2df8573a0054d4561e28fba63cdbf
Pedro Giampietro Reis <pedro.reis@cejam.local>
2021-09-18 16:43:04 -0300

    added a mongodb.

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
+  console.log('Connected to MongoDB! 🏓');
+});
 
 app.listen(3001, () => {
   console.log('API is running in port 3001! 👍');
