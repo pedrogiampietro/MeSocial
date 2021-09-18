e48db164c0562d3d721efe6d3dad5b1e027a50ce
Pedro Giampietro Reis <pedro.reis@cejam.local>
2021-09-18 16:50:46 -0300

    add middleware to routes

--- api/index.js
+++ api/index.js
@@ -6,10 +6,24 @@ const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
+const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
   console.log('Connected to MongoDB! 🏓');
+});
+
+//middlewares.
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'));
+app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
+
+app.get('/', (req, res) => {
+  res.send('OK');
 });
 
 app.listen(3001, () => {
