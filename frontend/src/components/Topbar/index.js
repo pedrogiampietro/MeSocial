4b68231ed1fc8d38bd94a8dfc04788533bbf4958
Pedro Giampietro Reis <pedro.reis@cejam.local>
2021-09-21 16:40:31 -0300

    finalized a topbar component

--- frontend/src/components/Topbar/index.js
+++ frontend/src/components/Topbar/index.js
@@ -1,5 +1,44 @@
 import React from 'react';
+import { Search, Person, Chat, Notifications } from '@material-ui/icons';
+
+import './styles.css';
 
 export function Topbar() {
-  return <h1>Topbar</h1>;
+  return (
+    <div className='topbarContainer'>
+      <div className='topbarLeft'>
+        <span className='logo'>MeSocial</span>
+      </div>
+      <div className='topbarCenter'>
+        <div className='searchBar'>
+          <Search className='searchIcon' />
+          <input
+            placeholder='Search for friend, post or video'
+            className='searchInput'
+          />
+        </div>
+      </div>
+      <div className='topbarRight'>
+        <div className='topbarLinks'>
+          <span className='topbarLink'>Homepage</span>
+          <span className='topbarLink'>Timeline</span>
+        </div>
+        <div className='topbarIcons'>
+          <div className='topbarIconItem'>
+            <Person />
+            <span className='topbarIconBadge'>1</span>
+          </div>
+          <div className='topbarIconItem'>
+            <Chat />
+            <span className='topbarIconBadge'>2</span>
+          </div>
+          <div className='topbarIconItem'>
+            <Notifications />
+            <span className='topbarIconBadge'>1</span>
+          </div>
+        </div>
+        <img src='/assets/person/1.jpeg' alt='' className='topbarImg' />
+      </div>
+    </div>
+  );
 }
