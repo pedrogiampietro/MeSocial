84420665a10b991b85762c2c0bde937c748782a2
Pedro Giampietro Reis <pedro.reis@cejam.local>
2021-09-21 16:13:29 -0300

    added pages, components, install material ui - core and icons

---
+++ frontend/src/components/Topbar/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function Topbar() {
+  return <h1>Topbar</h1>;
+}
---
+++ frontend/src/pages/Home/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Topbar } from '../../components/Topbar';
+
+export function Home() {
+  return (
+    <div>
+      <Topbar />
+    </div>
+  );
+}
