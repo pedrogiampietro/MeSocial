fe13daead3c5d73f249ef2875eef31ffb93f3774
Pedro Giampietro Reis <pedro.reis@cejam.local>
2021-09-21 17:36:19 -0300

    finalized pages.

---
+++ frontend/src/pages/Profile/index.js
@@ -0,0 +1,41 @@
+import { Topbar } from '../../components/Topbar';
+import { Sidebar } from '../../components/Sidebar';
+import { Feed } from '../../components/Feed';
+import { Rightbar } from '../../components/Rightbar';
+
+import './styles.css';
+
+export function Profile() {
+  return (
+    <>
+      <Topbar />
+      <div className='profile'>
+        <Sidebar />
+        <div className='profileRight'>
+          <div className='profileRightTop'>
+            <div className='profileCover'>
+              <img
+                className='profileCoverImg'
+                src='assets/post/3.jpeg'
+                alt=''
+              />
+              <img
+                className='profileUserImg'
+                src='assets/person/7.jpeg'
+                alt=''
+              />
+            </div>
+            <div className='profileInfo'>
+              <h4 className='profileInfoName'>Safak Kocaoglu</h4>
+              <span className='profileInfoDesc'>Hello my friends!</span>
+            </div>
+          </div>
+          <div className='profileRightBottom'>
+            <Feed />
+            <Rightbar profile />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
